49dad9539b37d13a10f51bf31731a78aa136330a
ttoper <tyler.toper@gmail.com>
2021-12-21 21:48:42 -0800

    Added screen drawing and renamed main to chess

--- chess.py
+++ chess.py
@@ -7,7 +7,7 @@ from pygame.locals import *
 # Global Static Variables
 screen_width=720
 screen_height=400
-game_name = 'Game Name'
+game_name = 'PyGame Chess'
 
 
 # Main function
@@ -29,20 +29,34 @@ def main():
 
     # Opens Display
     # Calls init if not inited
+    # This needs to be done before any events can be handle
     # size is window size
-    # depth is color depth, auto set to best settings if ignored
+    # depth is color depth, auto set to best settings. Leave it ignored
     window_surface = pygame.display.set_mode(size = [screen_width, screen_height])
 
 
     # Loop to play game
     running = True
     while running: 
+
         # Handle all queued events
         # If events are not used, they still need to be handled with pump()
         for event in pygame.event.get():
             # Game exit
             if event.type == QUIT:
                 running = False
+
+        # Set background color
+        # Currently fills space with black
+        window_surface.fill((0, 0, 0))
+
+        # Draw a rectangle
+        # Set to color red
+        # Draws a rectangle at (395, 0) with dimensions (10,10)
+        pygame.draw.rect(window_surface, (255, 0, 0), (395,0,10,10))
+
+        # Draw frame to screens
+        pygame.display.flip()
 
     # Close out pygame
     pygame.quit()
